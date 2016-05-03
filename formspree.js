d44aed6954c8349f8cc966f4581a32ab7d9017cb
ylliac <ylliac@hotmail.om>
2016-05-03 22:05:26 +0200

    Put some ajax in form validation

--- formspree.js
+++ formspree.js
@@ -2,7 +2,7 @@ var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '//formspree.io/your@email.com',
+		url: '//formspree.io/ylliac@hotmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
