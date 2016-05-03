5169ca869967af2c8a93cf5e85be987cc05a91f9
ylliac <ylliac@hotmail.om>
2016-05-03 22:00:29 +0200

    Put some ajax in form validation

--- formspree.js
+++ formspree.js
@@ -7,15 +7,15 @@ $contactForm.submit(function(e) {
 		data: $(this).serialize(),
 		dataType: 'json',
 		beforeSend: function() {
-			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+			$contactForm.append('<div class="alert alert--loading">Envoi du message...</div>');
 		},
 		success: function(data) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+			$contactForm.append('<div class="alert alert--success">Message envoyé !</div>');
 		},
 		error: function(err) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+			$contactForm.append('<div class="alert alert--error">Oups, le message n\'a pas été envoyé. Essayez le téléphone.</div>');
 		}
 	});
 });
