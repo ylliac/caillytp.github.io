e27057376c06678610ad2584c78ce4ee6b3f2ae8
ylliac <ylliac@hotmail.om>
2016-10-14 20:21:15 +0200

    fix - Reduce too large element and improve form feedback

--- formspree.js
+++ formspree.js
@@ -1,21 +1,35 @@
 var $contactForm = $('#contact-form');
+
 $contactForm.submit(function(e) {
 	e.preventDefault();
+	var $submit = $('button:submit', $contactForm);
+	var defaultSubmitText = $submit.text();
+
 	$.ajax({
 		url: '//formspree.io/ylliac@hotmail.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
 		beforeSend: function() {
-			$contactForm.append('<div class="alert alert--loading">Envoi du message...</div>');
+			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+			$submit.attr('disabled', true).text('Envoi en cours…');
 		},
 		success: function(data) {
-			$contactForm.find('.alert--loading').hide();
 			$contactForm.append('<div class="alert alert--success">Message envoyé !</div>');
+			$submit.text('Message envoyé !');
+			setTimeout(function() {
+				//$('.alert--success').remove();
+				$submit.attr('disabled', false).text(defaultSubmitText);
+			}, 5000);
 		},
 		error: function(err) {
-			$contactForm.find('.alert--loading').hide();
-			$contactForm.append('<div class="alert alert--error">Oups, le message n\'a pas été envoyé. Essayez le téléphone.</div>');
+			//$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Oops, il y a eu une erreur</div>');
+			$submit.text('Erreur, Cliquer pour réessayer');
+			setTimeout(function() {
+				//$('.alert--error').remove();
+				$submit.attr('disabled', false).text(defaultSubmitText);
+			}, 5000);
 		}
 	});
 });
